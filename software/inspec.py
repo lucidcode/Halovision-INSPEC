5feed10e073e3aa19917184d69bfb5fa2107d33c
Michael Paul Coder <mcoder@lucid-code.com>
2024-04-14 16:59:59 +1000

    Update settings via BLE

--- software/inspec.py
+++ software/inspec.py
@@ -18,48 +18,49 @@ class inspec_sensor:
         self.comms = inspec_comms()
         self.comms.message_received = self.ble_message_received
 
-        if self.config['Brightness']:
-            sensor.set_brightness(self.config['Brightness'])
-
-        if self.config['FrameSize'] == 'VGA':
-            sensor.set_framesize(sensor.VGA)
-        elif self.config['FrameSize'] == 'QVGA':
-            sensor.set_framesize(sensor.QVGA)
-        elif self.config['FrameSize'] == 'QQVGA':
-            sensor.set_framesize(sensor.QQVGA)
-        else:
-            sensor.set_framesize(sensor.QVGA)
-
-        if self.config['Color']:
-            sensor.set_pixformat(sensor.RGB565)
-            print("Color")
-            self.extra_fb = sensor.alloc_extra_fb(sensor.width(), sensor.height(), sensor.RGB565)
-        else:
-            sensor.set_pixformat(sensor.GRAYSCALE)
-            print("GRAYSCALE")
-            self.extra_fb = sensor.alloc_extra_fb(sensor.width(), sensor.height(), sensor.GRAYSCALE)
-
-        if self.config['AutoGain']:
-            sensor.set_auto_gain(True)
-        else:
-            sensor.set_auto_gain(False)
-
-        if self.config['AutoExposure']:
-            sensor.set_auto_exposure(True)
-        else:
-            sensor.set_auto_exposure(False)
+        self.configure_sensor()
 
         self.img = sensor.snapshot()
         self.extra_fb.replace(self.img)
         diff = self.img.variance(self.extra_fb, 128) / 100000
         
-        machine.RTC().datetime((self.config['Year'], self.config['Month'], self.config['Day'], 0, 0, 0, 0, 0))
+        machine.RTC().datetime((self.config.config['Year'], self.config.config['Month'], self.config.config['Day'], 0, 0, 0, 0, 0))
         
         self.lsd = lucid_scribe_data(self.config)
-        self.last_trigger = utime.ticks_ms() - self.config['TimeBetweenTriggers']
+        self.last_trigger = utime.ticks_ms() - self.config.config['TimeBetweenTriggers']
 
-        if self.config['Mode'] == "Research":
+        if self.config.config['Mode'] == "Research":
             self.stream = inspec_stream()
+
+    def configure_sensor(self):
+        if self.config.config['Brightness']:
+            sensor.set_brightness(self.config.config['Brightness'])
+
+        if self.config.config['FrameSize'] == 'VGA':
+            sensor.set_framesize(sensor.VGA)
+        elif self.config.config['FrameSize'] == 'QVGA':
+            sensor.set_framesize(sensor.QVGA)
+        elif self.config.config['FrameSize'] == 'QQVGA':
+            sensor.set_framesize(sensor.QQVGA)
+        else:
+            sensor.set_framesize(sensor.QVGA)
+
+        if self.config.config['PixelFormat'] == 'RGB565':
+            sensor.set_pixformat(sensor.RGB565)
+            self.extra_fb = sensor.alloc_extra_fb(sensor.width(), sensor.height(), sensor.RGB565)
+        if self.config.config['PixelFormat'] == 'Grayscale':
+            sensor.set_pixformat(sensor.GRAYSCALE)
+            self.extra_fb = sensor.alloc_extra_fb(sensor.width(), sensor.height(), sensor.GRAYSCALE)
+
+        if self.config.config['AutoGain']:
+            sensor.set_auto_gain(True)
+        else:
+            sensor.set_auto_gain(False)
+
+        if self.config.config['AutoExposure']:
+            sensor.set_auto_exposure(True)
+        else:
+            sensor.set_auto_exposure(False)
 
     def snapshot(self):
         self.img = sensor.snapshot()
@@ -71,7 +72,7 @@ class inspec_sensor:
         self.extra_fb.replace(self.img)
         self.lsd.log(int(self.diff))
 
-        if self.config['Mode'] == "Research":
+        if self.config.config['Mode'] == "Research":
             self.stream.send_image(self.img)
 
         self.comms.send_data("metrics", str(self.diff))
@@ -93,15 +94,21 @@ class inspec_sensor:
             directories = self.lsd.list_directories()
             self.comms.send_data("directories", directories)
 
+        if message.startswith("update.setting."):
+            message = message.replace('update.setting.', '')
+            setting, value = message.split(':')
+            self.config.config[setting] = value
+            self.config.save()
+            self.configure_sensor()
         
     def detect(self):
-        if self.config['Algorithm'] == "Motion Detection":
-            if self.diff > self.config['TriggerThreshold']:
+        if self.config.config['Algorithm'] == "Motion Detection":
+            if self.diff > self.config.config['TriggerThreshold']:
                 self.trigger()
         
     def trigger(self):
         now = utime.ticks_ms()
-        if (now - self.last_trigger > self.config['TimeBetweenTriggers']):
+        if (now - self.last_trigger > self.config.config['TimeBetweenTriggers']):
             self.comms.send_data("trigger", str(self.diff))
             self.comms.send_image(self.img)
             self.last_trigger = now
