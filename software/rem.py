e4fb0babc9523658685ff9f2d6f0b0a76df1122b
Michael Paul Coder <mcoder@lucid-code.com>
2024-06-07 19:31:11 +1000

    Add face tracking to rem detection

--- software/rem.py
+++ software/rem.py
@@ -1,15 +1,27 @@
 import utime
 
 class rapid_eye_movement:
-    def __init__(self, config):
+    def __init__(self, config, face):
         self.config = config
+        self.face = face
         self.eye_movements = 0
 
         now = utime.ticks_ms()
         self.last_eye_movement = now
 
     def dreaming(self, variance, trigger_threshold, toss_threshold):
+        if variance >= toss_threshold:
+            self.eye_movements = 0
+            print("eye_movement", self.eye_movements)
+
         now = utime.ticks_ms()
+        if now - self.last_eye_movement > 1000 * 60:
+            if self.eye_movements > 0:
+                self.eye_movements = self.eye_movements - 1
+                print("eye_movement", self.eye_movements)
+
+        if self.config.config['TrackFace'] and not self.face.has_face:
+            return self.eye_movements
 
         if variance >= trigger_threshold:
             if now - self.last_eye_movement > 1000:
@@ -17,14 +29,5 @@ class rapid_eye_movement:
                     self.eye_movements = self.eye_movements + 1
                     self.last_eye_movement = now
                     print("eye_movement", self.eye_movements)
-        
-        if variance >= toss_threshold:
-            self.eye_movements = 0
-            print("eye_movement", self.eye_movements)
-            
-        if now - self.last_eye_movement > 1000 * 60:
-            if self.eye_movements > 0:
-                self.eye_movements = self.eye_movements - 1
-                print("eye_movement", self.eye_movements)
 
         return self.eye_movements
