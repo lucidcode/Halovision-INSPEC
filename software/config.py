8c48437d78b41339602c83b43e9fad310bc1708b
Michael Paul Coder <mcoder@lucid-code.com>
2024-06-09 11:27:15 +1000

    Lower trigger threshold

--- software/config.py
+++ software/config.py
@@ -23,7 +23,7 @@ class inspec_config:
         self.config['Saturation'] = 0
         self.config['FrameSize'] = 'VGA'
         self.config['CreateLogs'] = 0
-        self.config['TriggerThreshold'] = 80
+        self.config['TriggerThreshold'] = 32
         self.config['TossThreshold'] = 8000
         self.config['TimeBetweenTriggers'] = 1000 * 60 * 1
         self.config['TimeBetweenTosses'] = 1000 * 60 * 5
