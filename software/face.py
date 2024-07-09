1d1a7043eabc54b8ed010f5478066a9985f23390
Michael Paul Coder <mcoder@lucid-code.com>
2024-07-09 16:07:22 +1000

    Optimize BLE throughput (#57)

--- software/face.py
+++ software/face.py
@@ -8,7 +8,6 @@ class face_detection:
         self.comms = comms
         self.has_face = False
         self.face_cascade = image.HaarCascade("frontalface", stages=self.config.get('FaceStages'))
-        self.last_change = utime.ticks_ms() - 1000
         self.face_object = [0, 0, 1, 1]
         
         self.extra_fb = sensor.alloc_extra_fb(sensor.width(), sensor.height(), sensor.GRAYSCALE)
@@ -18,7 +17,6 @@ class face_detection:
             return
 
         now = utime.ticks_ms()
-        has_face = False
         face_objects = img.find_features(self.face_cascade, threshold=self.config.get('FaceThreshold'), scale_factor=self.config.get('FaceScaleFactor'))
 
         if len(face_objects) == 0 and self.config.get('FaceAngles'):
@@ -31,11 +29,10 @@ class face_detection:
 
         if face_objects:
             self.face_object = face_objects[0]
-            has_face = True 
-            self.last_change = now
-            if self.has_face != has_face:
-                self.has_face = has_face
-                self.comms.send_data("face", "1")
+            self.has_face = True
+        else:
+            self.face_object = [0, 0, img.width(), img.height()]
+            self.has_face = False
         
         if self.config.get('DrawFaceRegion'):
             if self.face_object[2] != img.width():
@@ -43,11 +40,3 @@ class face_detection:
                     img.draw_rectangle(self.face_object, color=(70, 130, 180))
                 else:
                     img.draw_rectangle(self.face_object)
-
-        if now - self.last_change > 1000:
-            if not self.has_face:
-                self.face_object = [0, 0, img.width(), img.height()]
-            if self.has_face != has_face:
-                self.has_face = has_face
-                self.last_change = now
-                self.comms.send_data("face", "1" if self.has_face else "0")
