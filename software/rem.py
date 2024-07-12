6a8ff25e9aafff1858961c2938e3bcbe0779a02a
Michael Paul Coder <mcoder@lucid-code.com>
2024-07-13 09:41:52 +1000

    Remove debug logs

--- software/rem.py
+++ software/rem.py
@@ -10,13 +10,11 @@ class rapid_eye_movement:
     def detect(self, variance):
         if variance >= self.config.get('TossThreshold'):
             self.eye_movements = 0
-            print("eye_movement", self.eye_movements)
 
         now = utime.ticks_ms()
         if now - self.last_eye_movement > 1000 * 60:
             if self.eye_movements > 0:
                 self.eye_movements = self.eye_movements - 1
-                print("eye_movement", self.eye_movements)
 
         if self.config.get('TrackFace') and not self.face.has_face:
             return self.eye_movements
@@ -26,6 +24,5 @@ class rapid_eye_movement:
                 if self.eye_movements < 8:
                     self.eye_movements = self.eye_movements + 1
                     self.last_eye_movement = now
-                    print("eye_movement", self.eye_movements, "face", self.face.has_face)
 
         return self.eye_movements
