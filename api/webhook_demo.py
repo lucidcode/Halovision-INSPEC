fc41e4ca211f652fca54797db9573766424a2c3c
Michael Paul Coder <mcoder@lucid-code.com>
2024-11-26 11:55:25 +1100

    Add webhook api

---
+++ api/webhook_demo.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, abort
+import logging
+import datetime
+
+app = Flask(__name__)
+
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+@app.route('/trigger', methods=['POST'])
+def trigger():
+	if request.method == 'POST':
+		print(datetime.datetime.now(), request.json)
+		return 'success', 200
+	else:
+		abort(400)
+
+
+@app.route('/rem', methods=['POST'])
+def rem():
+	if request.method == 'POST':
+		print(datetime.datetime.now(), request.json)
+		return 'success', 200
+	else:
+		abort(400)
+
+
+@app.route('/variance', methods=['POST'])
+def variance():
+	if request.method == 'POST':
+		#print(datetime.datetime.now(), request.json)
+		return 'success', 200
+	else:
+		abort(400)
+
+
+if __name__ == '__main__':
+	app.run(host='localhost', port=5000)
