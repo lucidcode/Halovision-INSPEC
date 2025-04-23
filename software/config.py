376d2c0d7b65bfbe118878605e250c2fbd5a729f
Michael Paul Coder <mcoder@lucid-code.com>
2025-04-23 20:47:53 +0200

    Refactor face confidence

--- software/config.py
+++ software/config.py
@@ -37,7 +37,7 @@ class inspec_config:
         self.default['TrackFace'] = 1
         self.default['TrackEyes'] = 0
         self.default['TensorFlow'] = 1
-        self.default['MinConfidence'] = 102
+        self.default['FaceConfidence'] = 102
         self.default['FaceFeatures'] = 16
         self.default['DrawFaceRegion'] = 1
         self.default['FaceStages'] = 25
